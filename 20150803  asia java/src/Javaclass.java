edb304c6cf7660c8b55ae97c77fd315c9952facf
JoJoKTT <104021046@live.asia.edu.tw>
2015-08-03 14:39:07 +0800

    Firstwork2
    something change

--- 20150803  asia java/src/Javaclass.java
+++ 20150803  asia java/src/Javaclass.java
@@ -4,10 +4,10 @@ public class Javaclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            System.out.println("==============================");
-           System.out.println("*姓名：廖峻棋　　　　　　　　　　　　　　　　＊");
-           System.out.println("＊系所：亞洲大學資訊工程學系　　　　　　　　　＊");
-           System.out.println("＊職級：大0生　　　　　　　　　　　　　　　　＊");
-           System.out.println("＊電話：094145550　　　　　　　　　　　　＊");
+           System.out.println("＊姓名：廖峻棋　　　　　　　　　　　　　　　　＊");
+           System.out.println("＊系所：亞洲大學資訊工程學系　　　　　　＊");
+           System.out.println("＊職級：大0生　　　　　　　　　　　　　＊");
+           System.out.println("＊電話：094145550　　　　　　＊");
            System.out.println("==============================＊");
 	}
 
