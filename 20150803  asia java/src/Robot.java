e20142ce17d3deafca4974d163e5b6169d445414
JoJoKTT <104021046@live.asia.edu.tw>
2015-08-03 13:57:00 +0800

    Firstwork
    some pictures

---
+++ 20150803  asia java/src/Robot.java
@@ -0,0 +1,23 @@
+
+public class Robot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("¡@¡@¡@¢~¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¡X¢¡¡@¡@");
+           System.out.println("¡@¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@    ¢x¡@¡@");
+           System.out.println("¡@¡@¡@¢¥¢¤¢¤¢¤¢¤¢¤¢¤¢¤¡ó¢¤¡ó¢¤¢¤¢¤¢¤¢¤¢¤¢¤¢§¡@¡@");
+           System.out.println("¡@¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@    ¢x¡@¡@");
+           System.out.println("¡@¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@    ¡@¢x¡@¡@");
+           System.out.println("¡@¢~¡X¢t¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@ ¢u¡X¢¡");
+           System.out.println("¡@¢x¢~¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@   ¢x¢¡¢x");
+           System.out.println("¡@¢x¢x¡@¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p ¢x¢x");
+           System.out.println("¡@¢x¢x¡@¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p ¢x¢x");
+           System.out.println(" ¢¢¢£    ¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p  ¢¢¢£");
+           System.out.println("¡@¡@¡@  ¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¡@  ¡@");
+           System.out.println("¡@¡@¡@¡@¡@¡@¡@¢x¢x¡@¡@¡@¡@¡@¡@¢x¢x¡@¡@¡@¡@¡@¡@");
+           System.out.println("¡@¡@¡@¡@¡@¡@¡@¢x¢x¡@¡@¡@¡@¡@¡@¢x¢x¡@¡@¡@¡@¡@¡@");
+           System.out.println("¡@¡@¡@¡@   ¡@¢~ùöùö¢¡¡@¡@¡@   ¢~ùöùö¢¡¡@¡@¡@    ¡@");
+           System.out.println("¡@¡@¡@¡@¡@   ¢¢¡X¡X¡X¢£¡@¡@¡@     ¢¢¡X¡X¡X¢£¡@¡@¡@¡@¡@");
+	}
+
+}
