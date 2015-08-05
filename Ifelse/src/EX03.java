771cf41c01a0a134f369e3d1c2430af994bd8079
JoJoKTT <104021046@live.asia.edu.tw>
2015-08-05 10:11:43 +0800

    Update EX03.java

--- Ifelse/src/EX03.java
+++ Ifelse/src/EX03.java
@@ -3,7 +3,15 @@ public class EX03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+           Scanner scn = new Scanner(System.in);
+           int Θ = scn.nextInt();
+           int cm = scn.nextInt();
+           int h = scn.nextInt();
+           if(Θ>43 && cm>130 && h==12){
+        	   System.out.println("是");
+           }else{
+        	   System.out.println("不是");
+           }
 	}
 
 }
