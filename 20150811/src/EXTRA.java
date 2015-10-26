6fb1538211c56c861acd27cdab4287f2b2997cbf
JoJoKTT <104021046@live.asia.edu.tw>
2015-10-26 16:01:03 +0800

    20151026

--- 20150811/src/EXTRA.java
+++ 20150811/src/EXTRA.java
@@ -4,19 +4,21 @@ public class EXTRA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn=new Scanner(System.in);
-		int n=scn.nextInt();
-		int i=0;
-		while(i<n){
-			int j=0,k=0;
-			while(j<n-i-1){
-				System.out.print(" ");
-				j++;
+		while(true){
+			int n=scn.nextInt();
+			int i=0;
+			while(i<n){
+				int j=0,k=0;
+				while(j<n-i-1){
+					System.out.print(" ");
+					j++;
+				}
+				while(k<=i){
+					System.out.print("*");
+					k++;
+				}System.out.println();
+				i++;
 			}
-			while(k<=i){
-				System.out.print("*");
-				k++;
-			}System.out.println();
-			i++;
 		}
 	}
 
